b3a06796b047499e7b3f0886b2d67c45be459d9b
WebRenew <96023061+webrenew@users.noreply.github.com>
2022-05-26 20:55:42 -0400

    Initial Push

---
+++ design-sync/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  site: `https://skdz.webflow.io/`,
+  clientRouting: true,
+  optimizeImages: true,
+  optimizeJsLoading: true,
+  removeBranding: true,
+  minifyJs: false,
+  staticPageLimit: 200,
+  revalidate: false,
+}
